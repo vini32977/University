14240895fa4d884514231fd75a37f6e5e3b510ff
vini32977 <161340434+vini32977@users.noreply.github.com>
2024-09-10 15:13:01 +0530

    Add files via upload

---
+++ Python/python basics/Basic datatype in python.py
@@ -0,0 +1,36 @@
+a=10
+print("Type of a: ",type(a))
+print("Value of a: ",a)
+
+b=10.544
+print("Type of b: ",type(b))
+print("Value of b: ",b)
+
+c="Python"
+print("Type of c: ",type(c))
+print("Value of c: ",c)
+
+d=True
+print("Type of d: ",type(d))
+print("Value of d: ",d)
+
+e=[1,2,3,4,5,6]
+print("Type of e: ",type(e))
+print("Value of e: ",e)
+
+f=(1,2,3,4,5)
+print("Type of f: ",type(f))
+print("Value of f: ",f)
+
+g={1,2,3,4,5}
+print("Type of g: ",type(g))
+print("Value of g: ",g)
+
+h={"name":"vinay","age":"20"}
+print("Type of h: ",type(h))
+print("Value of h: ",h)
+
+i=None
+print("Type of h: ",type(a))
+print("Value of h: ",a)
+
